ba60b4dab77209d604e0e5c77a588b070bb516e0
Ben Kiboma <omayioben@gmail.com>
2022-09-02 21:00:23 +0300

    Test factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,4 +11,20 @@ describe Solver do
     end
   end
 
+  describe '#factorial' do
+    it 'Factorial of 0 should return 1' do
+      result = @solver.factorial(0)
+      expect(result).to be 1
+    end
+
+    it 'Factorial of 5 should return 120' do
+      result = @solver.factorial(5)
+      expect(result).to be 120
+    end
+
+    it 'Raise exception when number is negative' do
+      expect { @solver.factorial(-3) }.to raise_exception
+    end
+  end
+
 end
