12183ea85b0d0127101e20c9c7961066231e7e03
Ben Kiboma <omayioben@gmail.com>
2022-09-02 21:15:32 +0300

    Add factorial method

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(n)
+    return 1 if n.zero?
+    raise 'Exception' if n.negative?
+
+    factorial = 1
+    (2..n).each { |n| factorial *= n }
+    factorial
+  end
+
+end
